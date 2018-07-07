232c002f7c4ed043a5f61d6ce6853ca762a8d047
Shashank S <shash.iyer@gmail.com>
2018-07-07 12:42:31 +0530

    Print String
    this is a sample program to print the string values

---
+++ Basic/PrintString.java
@@ -0,0 +1,15 @@
+/*
+this program is to print the string values
+*/
+class PrintString 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("1000");
+		System.out.println("10.89");
+		System.out.println("a");
+		System.out.println("Y");
+		System.out.println("hello");
+		System.out.println("true");
+	}
+}
